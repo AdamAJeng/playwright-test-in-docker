13fde8e062a227137894f77e14b1a5f921767550
Adam A Jeng <adam.a.jeng@gmail.com>
2025-06-04 11:56:14 +0200

    headless: false

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 5,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: isCI
   ? [['blob']] // CI reporters
@@ -44,8 +44,8 @@ export default defineConfig({
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      /*name: 'webkit',
+      use: { ...devices['Desktop Safari'] },*/
     }] : 
     [{
       name: 'Google Chrome',
