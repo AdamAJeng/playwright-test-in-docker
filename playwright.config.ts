fe94f6534bdb952434fe59e091341802679a1a87
Adam A Jeng <adam.a.jeng@gmail.com>
2025-06-04 11:07:44 +0200

    update config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
+const isCI = !!process.env.CI;
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -16,13 +17,15 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly: isCI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: isCI
+  ? [['blob']] // CI reporters
+  : [['html', { open: 'always' }], ['list']], // Local reporters
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -30,24 +33,37 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: false
   },
 
   /* Configure projects for major browsers */
-  projects: [
+  projects: isCI ? [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
+    }] : 
+    [{
+      name: 'Google Chrome',
+      use: { 
+        ...devices['Desktop Chrome'],
+        channel: 'chrome',
+        viewport: { width: 2560, height: 1440 }
+      },
     },
+    {
+      name: 'Microsoft Edge',
+      use: { 
+        ...devices['Desktop Edge'], 
+        channel: 'msedge',
+        viewport: { width: 2560, height: 1440 }
+      },
+    }]
+
 
     /* Test against mobile viewports. */
     // {
@@ -68,7 +84,6 @@ export default defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
